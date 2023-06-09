f9da41be47791822e543f33e9462b6a851dd7fed
Bhavanabsf <136113044+Bhavanabsf@users.noreply.github.com>
2023-06-09 23:51:11 +0530

    Update streamlit_app.py
    Added content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('3 idlies')
+streamlit.text('hot water with leamon and honey')
+streaml.text('some nuts and dryfruits')
