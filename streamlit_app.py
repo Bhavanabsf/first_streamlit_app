dcb6a0b33c3b0f2288686f1e008be8d0daa81a9e
Bhavanabsf <136113044+Bhavanabsf@users.noreply.github.com>
2023-06-14 00:39:28 +0530

    Update streamlit_app.py
    Added another function for inserting data in snowflake db

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,13 +66,21 @@ if streamlit.button('Get Fruit List'):
 
 
 # don't run past anything here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+      return ("Thanks for adding "+ new_fruit)
 
-#This will not load data correctly but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.write(back_from_function)
+
+    
+
 
 
 
